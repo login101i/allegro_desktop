f3cbb60e1a897b75da65b96e4a7e495ae01c929a
login101i <maciejkruszyniak@gmail.com>
2022-01-30 22:13:48 +0100

    Testing counter app #1

--- tests/tests3/src/App.js
+++ tests/tests3/src/App.js
@@ -1,9 +1,43 @@
+import { useState } from "react";
+
 import "./App.css";
 
 function App() {
+	const [count, setCount] = useState(0);
+	const [error, setError] = useState(false);
+	console.log(count);
+	console.log(error);
+
+	const message = <span>You can not decrement below zero.</span>;
+
+	function handleIncrement() {
+		if (count >= 0) {
+			setCount(count + 1);
+			setError(false);
+		}
+	}
+
+	function handleDecrement() {
+		if (count <= 0) setError(true);
+		else {
+			setCount(count - 1);
+		}
+	}
+
 	return (
 		<div className="App" data-test="component-app">
-			<h1>App</h1>
+			<h1 data-test="counter-title">The counter display</h1>
+			<button data-test="decrement-button" onClick={handleDecrement}>
+				Decrement
+			</button>
+			<button data-test="increment-button" onClick={handleIncrement}>
+				Increment
+			</button>
+
+			<p data-test="counter-display">
+				<span data-test="count">{count}</span>
+			</p>
+			{error && message}
 		</div>
 	);
 }
--- tests/tests3/src/App.test.js
+++ tests/tests3/src/App.test.js
@@ -1,19 +1,63 @@
-import { render } from "@testing-library/react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * Factory function ot create a ShallowWrapper for the App Component
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+const setup = () => {
+	return shallow(<App />);
+};
+
+// why I cannot namt it "findByTestAtribute"? There is an error
+const searchByTestAtrr = (wrapper, val) => {
+	return wrapper.find(`[data-test="${val}"]`);
+};
 test("renders without crashing", () => {
-	const wrapper = shallow(<App />);
-	const appComponent = wrapper.find("[data-test='component-app']");
+	const wrapper = setup();
+	const appComponent = searchByTestAtrr(wrapper, "component-app");
 	expect(appComponent.length).toBe(1);
 });
 
-test("renders increment button", () => {});
+test("renders counter title", () => {
+	const wrapper = setup();
+	const counterTitle = searchByTestAtrr(wrapper, "counter-title");
+	expect(counterTitle.length).toBe(1);
+});
 
-test("renders counter display", () => {});
+test("renders increment button", () => {
+	const wrapper = setup();
+	const button = searchByTestAtrr(wrapper, "increment-button");
+	expect(button.length).toBe(1);
+});
 
-test("counter starts at 0", () => {});
-test("clicking button increments counter", () => {});
+test("renders properly  counter display", () => {
+	const wrapper = setup();
+	const counterDisplay = searchByTestAtrr(wrapper, "counter-display");
+	expect(counterDisplay.length).toBe(1);
+});
+
+test("counter starts at 0", () => {
+	const wrapper = setup();
+	const count = searchByTestAtrr(wrapper, "count").text();
+	expect(count).toBe("0");
+});
+
+test("clicking button increments counter", () => {
+	const wrapper = setup();
+	const button = searchByTestAtrr(wrapper, "increment-button");
+	button.simulate("click");
+	const count = searchByTestAtrr(wrapper, "count").text();
+	expect(count).toBe("1");
+});
+test("clicking button decrement counter", () => {
+	const wrapper = setup();
+	const button = searchByTestAtrr(wrapper, "decrement-button");
+	button.simulate("click");
+	const count = searchByTestAtrr(wrapper, "count").text();
+	expect(count).toBe("0");
+});
